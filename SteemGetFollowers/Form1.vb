1599b796a3c7dd7e4c4f3b914761ae06e207f216
moisesmcardona <moises@moises-studios.com>
2017-09-29 13:07:24 -0400

    Add project files.

---
+++ SteemGetFollowers/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim info As ProcessStartInfo = New ProcessStartInfo("bash", "-c ""python3 getFollowersCount.py " & TextBox1.Text & """") With {
+            .RedirectStandardOutput = True,
+            .RedirectStandardError = True,
+            .CreateNoWindow = True,
+            .UseShellExecute = False
+        }
+        Dim p As Process = Process.Start(info)
+        Dim output As String = p.StandardOutput.ReadToEnd.TrimEnd
+        MsgBox(TextBox1.Text & ", you have " & output & " followers.")
+    End Sub
+End Class
